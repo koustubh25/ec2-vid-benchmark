36bad19476ccf8d57f2ed79d6ff1e83604202be5
iomz <iomz@sfc.wide.ad.jp>
2013-11-19 15:54:22 -0800

    Inappropriate sudo in startbench.sh

--- startbench.sh
+++ startbench.sh
@@ -1,24 +1,26 @@
 #!/bin/sh
 # Prepare the dependencies
 if [ ! -e /etc/yum.repos.d/s3tools.repo ]; then
-  cd /etc/yum.repos.d && sudo wget http://s3tools.org/repo/RHEL_6/s3tools.repo
+  cd /etc/yum.repos.d && wget http://s3tools.org/repo/RHEL_6/s3tools.repo
 fi
 yum -y update
 yum -y install autoconf automake gcc gcc-c++ git libtool make nasm pkgconfig zlib-devel yasm s3cmd
 
+wait $!
 # Determine the instance type
 if [ -e /usr/bin/jq ]; then
   echo 'jq already installed'
 else
   cd ~
   wget http://stedolan.github.io/jq/download/linux64/jq
-  chmod +x ~/jq
-  sudo mv ~/jq /usr/bin/jq
+  chmod +x jq
+  mv jq /usr/bin/jq
   echo 'jq installed'
 fi
 IID=`wget -q -O - http://169.254.169.254/latest/meta-data/instance-id`
 echo `aws ec2 describe-instance-attribute --instance-id $IID --attribute instanceType | /usr/bin/jq '.InstanceType.Value' | sed -e 's/^"//'  -e 's/"$//'` > ~/InstanceType
 
+wait $!
 # libav
 if [ -e /usr/local/bin/avconv ]; then
   echo 'libav already installed'
@@ -30,6 +32,7 @@ else
   make && make install
 fi
 
+wait $!
 # x264benchmark
 if [ -e /usr/local/bin/x264 ]; then
   echo 'x264 already installed'
@@ -45,6 +48,7 @@ else
   make && make install
 fi
 
+wait $!
 # Run benchmark
 ITYPE=`cat ~/InstanceType`
 echo "Instance Type: $ITYPE"
@@ -53,4 +57,6 @@ cd ~/benchmark
 mkdir -p logs
 chmod +x launchbenchmark.sh
 #curl -O http://www.zumzocken.de/va_x264/elephantsdream_source.264
+
+wait $!
 /usr/bin/screen -d -m /usr/bin/time -o ~/benchmark/logs/$ITYPE.log ~/benchmark/launchbenchmark.sh $ITYPE
