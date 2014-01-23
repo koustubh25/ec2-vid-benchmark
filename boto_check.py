41bc693d1d6eb86b7d6c0dbfefeebeeccc281fde
iomz <iomz@sfc.wide.ad.jp>
2014-01-23 09:13:07 -0800

    DynamoDB table creation redunduncy upgrade

--- boto_check.py
+++ boto_check.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import sys
+import time
+
 try:
     from boto.dynamodb2.fields import HashKey, RangeKey
     from boto.dynamodb2.table import Table
@@ -8,11 +11,22 @@ except ImportError:
     sys.exit()
 
 instance_name = open('/var/local/instance_name').read().strip()
+
 # Check if the table exists or not, create one if not
+print "*** Connecting to DynamoDB '%s' table..." % instance_name
+table_struct = None
 try:
     logs = Table(instance_name, schema=[HashKey('trial'),RangeKey('parallel'),])
-    tmp = logs.describe()
+    table_struct = logs.describe()
 except JSONResponseError:
     logs = Table.create(instance_name, schema=[HashKey('trial'),RangeKey('parallel'),])
+    sys.stdout.write("*** DynamoDB is creating a new table...")
+while table_struct is None:
+    try:
+        logs = Table(instance_name, schema=[HashKey('trial'),RangeKey('parallel'),])
+        table_struct = logs.describe()
+        print "\tDone"
+    except JSONResponseError:
+        time.sleep(5)
 
 print 'boto'
--- ec2info_update.py
+++ ec2info_update.py
@@ -8,6 +8,7 @@ from HTMLParser import HTMLParser
 from pprint import pprint
 import json
 import sys
+import time
 import urllib
 
 # Retrieve instance types' detail
@@ -140,22 +141,27 @@ class VirtTypeParser(HTMLParser):
 
 def main():
     # Parse us-east linux instance prices for each gens
+    sys.stdout.write("*** Retrieving base price information for on-demand linux instances...")
     linux_od = "http://aws.amazon.com/ec2/pricing/json/linux-od.json"
     base_prices = {}
     price_dict = json.load(urllib.urlopen(linux_od))['config']['regions'][0]['instanceTypes']
     for gen in price_dict:
         for size in gen['sizes']:
             base_prices[size['size']] = size['valueColumns'][0]['prices']['USD']
+    print "\tDone"
             
     # Parse us-east linux ebs prices for each gens
+    sys.stdout.write("*** Retrieving EBS optimization price information...")
     pricing_ebs = "http://aws.amazon.com/ec2/pricing/pricing-ebs-optimized-instances.json"
     ebs_prices = {}
     ebs_dict = json.load(urllib.urlopen(pricing_ebs))['config']['regions'][0]['instanceTypes']
     for gen in ebs_dict:
         for size in gen['sizes']:
             ebs_prices[size['size']] = size['valueColumns'][0]['prices']['USD']
+    print "\tDone"
 
     # Retrieve instance type information
+    sys.stdout.write("*** Retrieving EC2 instance detail information...")
     types = "http://aws.amazon.com/ec2/instance-types/"
     parser = InstanceParser()
     try:
@@ -164,8 +170,10 @@ def main():
         pass
     instance_types = parser.arr
     cat = instance_types.pop(0)
+    print "\tDone"
     
     # Retrieve virtualization type information
+    sys.stdout.write("*** Retrieving instance virtualization type availability for Amazon Linux AMI...")
     types = "http://aws.amazon.com/amazon-linux-ami/instance-type-matrix/"
     vtp = VirtTypeParser()
     try:
@@ -173,19 +181,31 @@ def main():
         vtp.feed(urllib.urlopen(types).read().replace("&#x2713;", 'check'))
     except AssertionError:
         pass
+    print "\tDone"
     
     # Check if the table exists or not, create one if not
+    print "*** Connecting to DynamoDB 'ec2_instances' table..."
+    table_struct = None
     try:
         ec2_instances = Table('ec2_instances', schema=[HashKey('Instance Name'),])
-        tmp = ec2_instances.describe()
+        table_struct = ec2_instances.describe()
     except JSONResponseError:
         ec2_instances = Table.create('ec2_instances', schema=[HashKey('Instance Name'),])
+        sys.stdout.write("*** DynamoDB is creating a new table...")
+    while table_struct is None:
+        try:
+            ec2_instances = Table('ec2_instances', schema=[HashKey('Instance Name'),])
+            table_struct = ec2_instances.describe()
+            print "\tDone"
+        except JSONResponseError:
+            time.sleep(5)
     
     # Iterate all the instance combinations
     for i in instance_types:
         virt_types = vtp.get_virt_types(i[1])
         # Check if the instance is available in the lists
         if virt_types is None or i[1] not in base_prices or (i[7] =='Yes' and i[1] not in ebs_prices):
+            print '- %s not updated' % i[1]
             continue
         for vt in virt_types:
             ec2_prices = {}
@@ -210,12 +230,14 @@ def main():
                     'Instance Name': instance_name
                 }
                 if ec2_instances.has_item(**{'Instance Name': instance_name}):
-                    ec2_instances.delete_item(**{'Instance Name': instance_name})
+                    print "# %s already exists in the table" % instance_name
+                    continue
                 try:
-                    ec2_instances.put_item(data=instance)
-                    print "%s updated" % instance_name
+                    ec2_instances.put_item(data=instance, overwrite=True)
+                    print "+ %s updated" % instance_name
                 except ConditionalCheckFailedException:
-                    print '%s not updated' % instance_name
+                    print "- %s not updated" % instance_name
+    print "\n*** 'ec2-instances' table is now fully updated!"
 
 if __name__ == "__main__":
     main()
